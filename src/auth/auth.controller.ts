09dae59668329b53e57382fc18ee9b24b25aae22
Roseweltty Guida <feijaoguida@hotmail.com>
2025-02-13 21:26:54 -0300

    feat: Guards, Local Stratagy and jwt implements

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+  constructor(private readonly AuthService: AuthService) {}
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(LocalAuthGuard)
+  login(@Request() req: AuthRequest) {
+    return this.AuthService.login(req.user);
+  }
+}
